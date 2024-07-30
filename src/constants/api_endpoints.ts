eb1b23d343e861a5c9a5884430e5a71640ab7ba0
Pranay Payal <58637121+thepranays@users.noreply.github.com>
2024-07-30 12:56:09 +0530

    changes to hostname in api endpoints

--- src/constants/api_endpoints.ts
+++ src/constants/api_endpoints.ts
@@ -1,7 +1,7 @@
-export const api_userServiceEndpoint = "http://localhost:8081/api/user";
-export const api_productServiceEndpoint = "http://localhost:8081/api/product";
-export const api_conversationServiceEndpoint = "http://localhost:8081/api/chat/conversation";
-export const api_messageServiceEndpoint = "http://localhost:8081/api/chat/message";
-export const api_chatServiceEndpoint = "http://localhost:8081/api/chat";
-export const api_notificationServiceEndpoint = "http://localhost:8081/api/notification";
-export const ws_chatServiceEndpoint = "ws://localhost:8081/ws/vidyarthi-ws-chat";
+export const api_userServiceEndpoint = "http://api-gateway:8081/api/user";
+export const api_productServiceEndpoint = "http://api-gateway:8081/api/product";
+export const api_conversationServiceEndpoint = "http://api-gateway:8081/api/chat/conversation";
+export const api_messageServiceEndpoint = "http://api-gateway:8081/api/chat/message";
+export const api_chatServiceEndpoint = "http://api-gateway:8081/api/chat";
+export const api_notificationServiceEndpoint = "http://api-gateway:8081/api/notification";
+export const ws_chatServiceEndpoint = "ws://api-gateway:8081/ws/vidyarthi-ws-chat";
